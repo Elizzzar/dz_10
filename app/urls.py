401ed0323de2ecadc1344e1dda0e1442b8ba078c
Turganbek Elizar <turganbekelizar22@gmail.com>
2023-10-23 01:05:45 +0600

    dz_10

--- app/urls.py
+++ app/urls.py
@@ -17,3 +17,10 @@ urlpatterns = [
     # Страница удаления задачи и тоже принимает идентификатор
     path('<int:pk>/delete/', TaskDeleteView.as_view(), name='task_delete'),
 ]
+# urlpatterns = [
+#     re_path(r'^$', TaskListView.as_view(), name='task_list'),
+#     re_path(r'^create$', TaskCreateView.as_view(), name='task_create'),
+#     re_path(r'^(?P<pk>\d+)/$' ,TaskDetailView.as_view(), name='task_detail'),
+#     re_path(r'^(?P<pk>\d+)/update$' ,TaskUpdateView.as_view(), name='task_update'),
+#     re_path(r'^(?P<pk>\d+)/delete$' ,TaskDeleteView.as_view(), name='task_delete'),
+# ]
